e309b3f04627b5c87d7730558fced1ef015d87b8
Jeremy 'LeJyBy' Barbay <jeremy@barbay.cl>
2016-03-13 20:59:22 -0300

    Added documentation to python code.

--- Python/radioJyBy.py
+++ Python/radioJyBy.py
@@ -1,9 +1,21 @@
+"""Radio JyBy
+Plays your local music (music player like) while interleaving it with recorded personal comments (DJ like) to simulate the feel of a radio. 
+
+Prototype in Python for desktop.
+"""
+
 import time, wave, pymedia.audio.sound as sound
-f= wave.open( '../RadioComments/5mnBreak-HoYouHaveBeenWorkingGreatNoItSTimeForAFiveMinuteBreak.wav', 'rb' )
+
+MusicFolder = '../Music/'
+RadioCommentsFolder = '../RadioComments/'
+
+f= wave.open( RadioCommentsFolder+'5mnBreak-HoYouHaveBeenWorkingGreatNoItSTimeForAFiveMinuteBreak.wav', 'rb' )
+
 sampleRate= f.getframerate()
 channels= f.getnchannels()
 format= sound.AFMT_S16_LE
 snd= sound.Output( sampleRate, channels, format )
 s= f.readframes( 300000 )
+
 snd.play( s )
 while snd.isPlaying(): time.sleep( 0.05 )
